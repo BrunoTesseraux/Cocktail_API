70ffbcec5453c1471f4bce944856590176bbd7e6
BrunoTesseraux <bruno.tesseraux@gmx.de>
2023-12-13 09:09:01 +0100

    boxshadow

--- src/components/CocktailList/CocktailList.jsx
+++ src/components/CocktailList/CocktailList.jsx
@@ -1,27 +1,28 @@
 const CocktailList = () => {
-  document.addEventListener("DOMContentLoaded", function () {
-    var polaroids = document.querySelectorAll(".polaroid");
+  const style = () => {
+    const polaroids = document.querySelectorAll(".polaroid");
 
     polaroids.forEach(function (polaroid) {
-      var randomRotation = Math.floor(Math.random() * 11) - 5; // Zufälliger Winkel zwischen -5 und 5 Grad
-      var randomXOffset = Math.floor(Math.random() * 21) - 10; // Zufällige Verschiebung zwischen -10px und 10px horizontal
-      var randomYOffset = Math.floor(Math.random() * 21) - 10; // Zufällige Verschiebung zwischen -10px und 10px vertikal
+      const randomRotation = Math.floor(Math.random() * 11) - 5; // Zufälliger Winkel zwischen -5 und 5 Grad
+      const randomXOffset = Math.floor(Math.random() * 21) - 10; // Zufällige Verschiebung zwischen -10px und 10px horizontal
+      const randomYOffset = Math.floor(Math.random() * 21) - 10; // Zufällige Verschiebung zwischen -10px und 10px vertikal
 
-      var transformValue =
+      const transformValue =
         "rotate(" +
         randomRotation +
-        "deg) translate(" +
+        "deg) translateX(" +
         randomXOffset +
-        "px, " +
+        "px) translateY(" +
         randomYOffset +
         "px)";
 
       polaroid.style.transform = transformValue;
     });
-  });
+  };
 
   return (
     <section>
+      <button onClick={style()}>Style</button>
       <div className="polaroid">
         <div className="black"></div>
       </div>
