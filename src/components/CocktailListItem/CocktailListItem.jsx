77f38c147013c4570f62ce0d0a793466b9fa48bb
YacineYldrm <yacine@mailbox.org>
2023-12-13 20:44:03 +0100

    popup function in cocktaillistitem eingebaut. noch nicht fertig

--- src/components/CocktailListItem/CocktailListItem.jsx
+++ src/components/CocktailListItem/CocktailListItem.jsx
@@ -3,12 +3,24 @@ import "./CocktailListItem.scss";
 
 const CocktailListItem = (props) => {
 
+  // When the user clicks on div, open the popup
+  const popupFunction = () => {
+
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`)
+          .then(response => response.json())
+          .then(data => console.log(data.drinks));
+          
+      var popup = document.getElementById("myPopup");
+      popup.textContent = `POPOPOPOPUP`
+      popup.classList.toggle("show");
+  }
+
   return (
-    <div key={props.id} className="polaroid">
-      <Link to={`./${props.id}`}>
+    <div onClick={popupFunction} key={props.id} className="polaroid popup">
+      <div className="popuptext" id="myPopup">
         <img src={props.img} alt={props.name} />
         <h3>{props.name}</h3>
-      </Link>
+      </div>
     </div>
   );
 };
