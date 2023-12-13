37193150f7dd8117cda0f603eba80ed0883610ea
BrunoTesseraux <bruno.tesseraux@gmx.de>
2023-12-13 20:43:09 +0100

    details struktur gebaut

--- src/components/CocktailDetails/CocktailDetails.jsx
+++ src/components/CocktailDetails/CocktailDetails.jsx
@@ -1,27 +1,38 @@
 import "./CocktailDetails.scss";
 
 const CocktailDetails = () => {
-
   return (
     <>
       <div class="card">
         <div class="imgBox">
-          <div class="bark"></div>
-          <img src="https://image.ibb.co/fYzTrb/lastofus.jpg" />
+          <h2 class="bark">Cocktailname</h2>
+          <img src="Bild" />
         </div>
         <div class="details">
-          <h4 class="color1">You're not a Fossil! (YET)</h4>
-          <h4 class="color2 margin">(HAPPY BIRTHDAY)</h4>
-          <p>Dear Dad,</p>
-          <p>Let's see.. .</p>
-          <p>You’re never around, you</p>
-          <p>hate the music I’m into, you</p>
-          <p>practically despise the movies I</p>
-          <p>like, and yet somehow you still</p>
-          <p>manage to be the best dad every year.</p>
-          <p>How do you do that? :</p>
-          <p class="text-right">Happy Birthday, papa!</p>
-          <p class="text-right">♥Sarah</p>
+          <h4>
+            ingredient1 <span>measure1</span>
+          </h4>
+          <h4>
+            ingredient2 <span>measure2</span>
+          </h4>
+          <h4>
+            ingredient3 <span>measure3</span>
+          </h4>
+          <h4>
+            ingredient4 <span>measure4</span>
+          </h4>
+          <h4>
+            ingredient5 <span>measure5</span>
+          </h4>
+          <h4>
+            ingredient6 <span>measure6</span>
+          </h4>
+          <h4>
+            ingredient7 <span>measure7</span>
+          </h4>
+          <p>Instructions</p>
+          {/* <p class="text-right">Happy Birthday, papa!</p>
+          <p class="text-right">♥Sarah</p> */}
         </div>
       </div>
     </>
