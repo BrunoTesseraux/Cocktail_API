cb5136c25ea943ba4e57af1d82da54199d9d5529
canyus <canyus@gmx.de>
2023-12-13 09:16:30 +0100

    4 von 5 fetch stehen

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Footer from "./components/footer/footer";
 import Home from "./page/Home";
 import CocktailList from "./components/CocktailList/CocktailList";
 import CocktailDetails from "./components/CocktailDetails/CocktailDetails";
+import Fetch from "./page/Fetch";
+import Fetch2 from "./page/Fetch2";
+import Fetch3 from "./page/Fetch3";
+import Fetch4 from "./page/Fetch4";
+import Fetch5 from "./page/Fetch5";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+          <Fetch/>
+          <Fetch2/>
+          <Fetch3/>
+          <Fetch4/>
+          <Fetch5/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cocktails" element={<CocktailList />} />
---
+++ src/page/Fetch3.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const Fetch3 = () => {
+    const [getData, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+// by ingredient
+    useEffect(() => {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setData(data.drinks);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <section>
+            {getData?.map((cock, index) => (
+                <div key={index}>{cock.strDrink}</div>
+            ))}
+        </section>
+    );
+}
+
+export default Fetch3;
