ec7e5677304dded739960a990373600056636df8
BrunoTesseraux <bruno.tesseraux@gmx.de>
2023-12-13 22:15:38 +0100

    detailpage

--- src/components/CocktailListItem/CocktailListItem.jsx
+++ src/components/CocktailListItem/CocktailListItem.jsx
@@ -1,26 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import CocktailDetails from "../CocktailDetails/CocktailDetails";
 import "./CocktailListItem.scss";
 
-const CocktailListItem = ({key, id, name, img}) => {
-
+const CocktailListItem = ({ key, id, name, img }) => {
+  console.log(id);
+  const [cocktail, setCocktail] = useState([]);
+  console.log(cocktail[0]?.strIngredient1);
   // When the user clicks on div, open the popup
   const popupFunction = () => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then((response) => response.json())
+      .then((data) => setCocktail([...cocktail, data.drinks[0]]));
 
-      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`)
-          .then(response => response.json())
-          .then(data => console.log(data.drinks));
-          
-      let popup = document.getElementById(id);
-      popup.textContent = `POPOPOPOPUP`
-      popup.classList.toggle = "show";
-  }
+    let popup = document.getElementById(id);
+    // popup.textContent = `POPOPOPOPUP`;
+    popup.classList.toggle("show");
+  };
 
   return (
     <div onClick={popupFunction} key={id} className="polaroid popup">
-      <div id={id}>
-        <img src={img} alt={name} />
-        <h3>{name}</h3>
-      </div>
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
+      <span className="popuptext" id={id}>
+        <CocktailDetails
+          name={cocktail[0]?.strDrink}
+          img={cocktail[0]?.strDrinkThumb}
+          beschreibung={cocktail[0]?.strInstructionsDE}
+          zutat1={cocktail[0]?.strIngredient1}
+          zutat2={cocktail[0]?.strIngredient2}
+          zutat3={cocktail[0]?.strIngredient3}
+          zutat4={cocktail[0]?.strIngredient4}
+          zutat5={cocktail[0]?.strIngredient5}
+          measure1={cocktail[0]?.strMeasure1}
+          measure2={cocktail[0]?.strMeasure2}
+          measure3={cocktail[0]?.strMeasure3}
+          measure4={cocktail[0]?.strMeasure4}
+          measure5={cocktail[0]?.strMeasure5}
+        />
+      </span>
     </div>
   );
 };
