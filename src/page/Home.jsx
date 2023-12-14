72541aa931a125107ff99d111b3971ee27de6db7
BrunoTesseraux <bruno.tesseraux@gmx.de>
2023-12-14 09:11:02 +0100

    all ingredients wrapper

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,35 +3,62 @@ import "./Home.scss";
 import { useContext, useState } from "react";
 import { AllIngredientsContext } from "../components/Context/Context";
 const Home = () => {
-
   const allIngredientsContext = useContext(AllIngredientsContext);
-  const allIngredientsState =  allIngredientsContext.allIngredients[0];
+  const allIngredientsState = allIngredientsContext.allIngredients[0];
   const [cocktailName, setCocktailName] = useState("");
 
   return (
     <>
       {/* Gin */}
       <div className="link_container">
-        <Link className="link" onMouseEnter={(event) => setCocktailName(event.target.textContent)} to={`/cocktails/${cocktailName.trim().toLowerCase()}`}><div className="test"><img src="" alt="" /> <h3>Gin</h3> </div></Link>
-        </div>
+        <Link
+          className="link"
+          onMouseEnter={(event) => setCocktailName(event.target.textContent)}
+          to={`/cocktails/${cocktailName.trim().toLowerCase()}`}
+        >
+          <div className="test">
+            <img src="" alt="" /> <h3>Gin</h3>{" "}
+          </div>
+        </Link>
+      </div>
 
       {/* Vodka */}
       <div className="link_container">
-        <Link className="link" onMouseEnter={(event) => setCocktailName(event.target.textContent)} to={`/cocktails/${cocktailName.trim().toLowerCase()}`}><div className="test"><img src="" alt="" /> <h3>Vodka</h3> </div></Link>
-        </div>
+        <Link
+          className="link"
+          onMouseEnter={(event) => setCocktailName(event.target.textContent)}
+          to={`/cocktails/${cocktailName.trim().toLowerCase()}`}
+        >
+          <div className="test">
+            <img src="" alt="" /> <h3>Vodka</h3>{" "}
+          </div>
+        </Link>
+      </div>
 
       {/* Rum */}
       <div className="link_container">
-        <Link className="link" onMouseEnter={(event) => setCocktailName(event.target.textContent)} to={`/cocktails/${cocktailName.trim().toLowerCase()}`}><div className="test"><img src="" alt="" /> <h3>Rum</h3> </div></Link>
-        </div>
+        <Link
+          className="link"
+          onMouseEnter={(event) => setCocktailName(event.target.textContent)}
+          to={`/cocktails/${cocktailName.trim().toLowerCase()}`}
+        >
+          <div className="test">
+            <img src="" alt="" /> <h3>Rum</h3>{" "}
+          </div>
+        </Link>
+      </div>
 
       {/* All ingredients */}
       <div className="all-ingredients-container">
-        {allIngredientsState?.map((ingredient, index) => 
-         <Link key={index} onMouseEnter={(event) => setCocktailName(event.target.textContent)} to={`/cocktails/${cocktailName.trim().toLowerCase()}`}>
-          <h2>{ingredient.strIngredient1}</h2></Link>
-
-)}
+        {allIngredientsState?.map((ingredient, index) => (
+          <Link
+            key={index}
+            onMouseEnter={(event) => setCocktailName(event.target.textContent)}
+            to={`/cocktails/${cocktailName.trim().toLowerCase()}`}
+          >
+            <h2>{ingredient.strIngredient1}</h2>
+          </Link>
+        ))}
       </div>
 
       {/* Alkoholfrei */}
